141644ade62a52a72ad322a61bba43e39a6b4ee1
chintan <chintanvaghani555@gmail.com>
2025-07-13 15:25:42 +0200

    changed zitadel config

--- src/auth/zitadelConfig.ts
+++ src/auth/zitadelConfig.ts
@@ -3,13 +3,13 @@ import { UserManager, UserManagerSettings } from 'oidc-client-ts';
 const zitadelConfig: UserManagerSettings = {
   authority: 'https://one-touch-xfjetu.us1.zitadel.cloud/',
   client_id: '328120241748011721',
-  redirect_uri: 'http://localhost:5173/',
-  post_logout_redirect_uri: 'http://localhost:5173/',
+  redirect_uri: 'https://one-touch-frontend.vercel.app/',
+  post_logout_redirect_uri: 'https://one-touch-frontend.vercel.app/',
   response_type: 'code',
   scope: 'openid profile email urn:zitadel:iam:org:project:roles',
   loadUserInfo: true,
   automaticSilentRenew: true,
-  silent_redirect_uri: 'http://localhost:5173/silent-renew.html',
+  silent_redirect_uri: 'https://one-touch-frontend.vercel.app/silent-renew.html',
   monitorSession: false,
 };
 
