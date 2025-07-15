9427d586894770a98f69cf2cf735223380444e45
chintan <chintanvaghani555@gmail.com>
2025-07-15 20:38:58 +0200

    changed redirect url in zitadel

--- src/auth/zitadelConfig.ts
+++ src/auth/zitadelConfig.ts
@@ -3,13 +3,13 @@ import { UserManager, UserManagerSettings } from 'oidc-client-ts';
 const zitadelConfig: UserManagerSettings = {
   authority: import.meta.env.VITE_ZITADEL_AUTHORITY,
   client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-  redirect_uri: 'http://localhost:5173/',
-  post_logout_redirect_uri: 'http://localhost:5173/',
+  redirect_uri: 'https://one-touch-frontend.vercel.app/',
+  post_logout_redirect_uri: 'https://one-touch-frontend.vercel.app/',
   response_type: 'code',
   scope: 'openid profile email urn:zitadel:iam:org:project:roles',
   loadUserInfo: true,
   automaticSilentRenew: true,
-  silent_redirect_uri: 'http://localhost:5173/silent-renew.html',
+  silent_redirect_uri: 'https://one-touch-frontend.vercel.app/silent-renew.html',
   monitorSession: false,
 };
 
