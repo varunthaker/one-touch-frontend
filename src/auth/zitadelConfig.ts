f8c16630bde615ac3035dbe56aeb711a7bf18cd4
ravisurelia <iamravisurelia@gmail.com>
2025-07-22 19:10:09 +0200

    zitadel config changed to prod credentials

--- src/auth/zitadelConfig.ts
+++ src/auth/zitadelConfig.ts
@@ -1,36 +1,19 @@
 import { UserManager, UserManagerSettings } from "oidc-client-ts";
 
+// Note: The below code is for live deployment. Uncomment and use it when deploying to production.
 const zitadelConfig: UserManagerSettings = {
   authority: import.meta.env.VITE_ZITADEL_AUTHORITY,
   client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-  redirect_uri: "http://localhost:5173/",
-  post_logout_redirect_uri: "http://localhost:5173/",
+  redirect_uri: "https://onetouchpro.app/",
+  post_logout_redirect_uri: "https://onetouchpro.app/",
   response_type: "code",
   scope: "openid profile email urn:zitadel:iam:org:project:roles",
   loadUserInfo: true,
   automaticSilentRenew: true,
-  silent_redirect_uri: "http://localhost:5173/silent-renew.html",
+  silent_redirect_uri: "https://onetouchpro.app/silent-renew.html",
   monitorSession: false,
 };
 
 export const userManager = new UserManager(zitadelConfig);
 
 export default zitadelConfig;
-
-//Note: The below code is for live deployment. Uncomment and use it when deploying to production.
-// const zitadelConfig: UserManagerSettings = {
-//   authority: import.meta.env.VITE_ZITADEL_AUTHORITY,
-//   client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-//   redirect_uri: 'https://onetouchpro.app/',
-//   post_logout_redirect_uri: 'https://onetouchpro.app/',
-//   response_type: 'code',
-//   scope: 'openid profile email urn:zitadel:iam:org:project:roles',
-//   loadUserInfo: true,
-//   automaticSilentRenew: true,
-//   silent_redirect_uri: 'https://onetouchpro.app/silent-renew.html',
-//   monitorSession: false,
-// };
-
-// export const userManager = new UserManager(zitadelConfig);
-
-// export default zitadelConfig;
