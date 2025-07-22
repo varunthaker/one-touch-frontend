f514e244cf47190e58143debb8454ec6ac05b67e
Varun <varun.thaker@aiio.de>
2025-07-22 17:01:51 +0200

    created prod docker and fixed env things

--- src/auth/zitadelConfig.ts
+++ src/auth/zitadelConfig.ts
@@ -1,18 +1,35 @@
 import { UserManager, UserManagerSettings } from 'oidc-client-ts';
 
+const authority = import.meta.env.VITE_ZITADEL_AUTHORITY;
+const client_id = import.meta.env.VITE_ZITADEL_CLIENT_ID;
+const redirect_uri = import.meta.env.VITE_ZITADEL_REDIRECT_URL;
+
+// Check if required values are present
+if (!authority) {
+  console.error('VITE_ZITADEL_AUTHORITY is not defined in environment variables');
+}
+
+if (!client_id) {
+  console.error('VITE_ZITADEL_CLIENT_ID is not defined in environment variables');
+}
+
+if (!redirect_uri) {
+  console.error('VITE_ZITADEL_REDIRECT_URL is not defined in environment variables');
+}
+
 const zitadelConfig: UserManagerSettings = {
-  authority: import.meta.env.VITE_ZITADEL_AUTHORITY,
-  client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-  redirect_uri: 'https://onetouchpro.app/',
-  post_logout_redirect_uri: 'https://onetouchpro.app/',
+  authority,
+  client_id,
+  redirect_uri,
+  post_logout_redirect_uri: import.meta.env.VITE_ZITADEL_REDIRECT_URL,
   response_type: 'code',
   scope: 'openid profile email urn:zitadel:iam:org:project:roles',
   loadUserInfo: true,
   automaticSilentRenew: true,
-  silent_redirect_uri: 'https://onetouchpro.app/silent-renew.html',
+  silent_redirect_uri: import.meta.env.VITE_SILENT_REDIRECT_URI,
   monitorSession: false,
 };
 
 export const userManager = new UserManager(zitadelConfig);
 
-export default zitadelConfig; 
+export default zitadelConfig;
